6c4549e79173274b3f5149e9a9f8c9aa23f21c55
Artem Kvitka <kvitka.artem99@gmail.com>
2024-06-11 15:37:27 +0300

    Small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,22 @@ import './App.css';
 const tg = window.Telegram.WebApp;
 
 function App() {
+  
   useEffect(() => {
+    console.log(tg);
     tg.ready();
   }, []);
 
   const onClose = () => {
+    console.log("test");
+
     tg.close();
   };
 
   return (
     <div>
       Work
-      <button onClick={onClose}>Close</button>
+      <button onClick={onClose}>Test my app online</button>
     </div>
   );
 }
